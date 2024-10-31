87f7ee41c09ea352be56fcca669d297c07177ae9
Barmanji <barmanjiaj@gmail.com>
2024-10-31 22:30:57 +0530

    update: messageForm

--- app/components/messageForm.tsx
+++ app/components/messageForm.tsx
@@ -1,15 +1,14 @@
 "use client"
 import { TextareaAutosize, Snackbar, Alert } from "@mui/material";
 import { useState } from 'react';
-import { MessageSquare, User, Mail, Send } from "lucide-react";
+import { MessageSquare, User, Mail, Phone, Send } from "lucide-react";
 import { useForm } from '@formspree/react';
+import { PhoneInput } from "react-international-phone";
 
-import 'react-international-phone/style.css';
-import '../styling/phoneField.css';
 import { Card } from "../components/card";
 
 export default function MessageForm() {
-    const [formData, setFormData] = useState({ message: "", name: "", email: "" });
+    const [formData, setFormData] = useState({ message: "", name: "", email: ""});
     const [state, handleSubmit] = useForm(process.env.NEXT_PUBLIC_FORMSPREE_TOKEN || "");
     const [showMessageSentAlert, setShowMessageSentAlert] = useState(false);
     const [showEmailInvalidAlert, setShowEmailInvalidAlert] = useState(false);
@@ -34,7 +33,7 @@ export default function MessageForm() {
     }
 
     const resetForm = () => {
-        setFormData({ message: "", name: "", email: "" });
+        setFormData({ message: "", name: "", email: ""});
     }
 
     const isEmailValid = (email: string) => {
@@ -62,42 +61,46 @@ export default function MessageForm() {
             >
                 <div className="grid w-full grid-cols-1 xs:grid-cols-1 sm:mt-0 sm:grid-cols-1 md:grid-cols-3 lg:gap-5 lg:grid-cols-3 xl:grid-cols-3">
                     <div className="flex flex-col gap-2 col-span-2 md:mr-5 mb-5 md:mb-0">
-                        <div className="text-white flex flex-row items-center gap-2 font-kode-mono" style={{ fontSize: 15 }}>
-                            <MessageSquare size={20} className="text-sea-green" />Message
-                        </div>
+                        <div className="text-white flex flex-row items-center gap-2 font-kode-mono" style={{ fontSize: 15 }}><MessageSquare
+                            size={20}
+                            className="text-sea-green"
+                        />Message</div>
                         <TextareaAutosize
                             value={formData.message}
                             minRows={6}
+                            maxRows={10}
                             style={{
                                 overflow: "auto",
-                                color: "#ffffff",
+                                color: "white", // Change text color to white
                                 backgroundColor: "#1f1f1f",
                                 border: "1px solid #6c6c74",
                                 padding: 10,
                                 borderRadius: 5,
                                 fontSize: "small",
                                 width: "100%",
-                                height: "calc(7% - 20px)", // Adjust height to fill the container
-                                boxSizing: "border-box",
-                            }}
-                            className="font-kode-mono placeholder-input"
+                                height: "calc(7% - 20px)", // Keep height calculation
+                                boxSizing: "border-box", // Include padding in height calculation
+                            }}                            className="font-kode-mono placeholder-input"
                             id="message"
                             name="message"
                             placeholder="Type your message here"
-                            required
+                            required={true}
                             onChange={(e) => setFormData({ ...formData, message: e.target.value })}
                         />
                     </div>
                     <div className="flex flex-col gap-5 col-span-1 items-center w-full">
                         <div className="flex flex-col gap-1 w-full">
                             <div className="text-white flex flex-row items-center gap-2 font-kode-mono" style={{ fontSize: 15 }}>
-                                <User size={20} className="text-sea-green" />Name
+                                <User
+                                    size={20}
+                                    className="text-sea-green"
+                                />Name
                             </div>
                             <input
                                 value={formData.name}
                                 type="text"
                                 name="name"
-                                required
+                                required={true}
                                 placeholder="Full Name"
                                 style={{
                                     overflow: "auto",
@@ -114,15 +117,12 @@ export default function MessageForm() {
                             />
                         </div>
                         <div className="flex flex-col gap-1 w-full">
-                            <div className="text-white flex flex-row items-center gap-2 font-kode-mono" style={{ fontSize: 15 }}>
-                                <Mail size={20} className="text-sea-green" />Email
-                            </div>
+                            <div className="text-white flex flex-row items-center gap-2 font-kode-mono" style={{ fontSize: 15 }}><Mail
+                                size={20}
+                                className="text-sea-green"
+                            />Email</div>
                             <input
                                 value={formData.email}
-                                type="email"
-                                name="email"
-                                required
-                                placeholder="Email Address"
                                 style={{
                                     overflow: "auto",
                                     color: "#ffffff",
@@ -134,41 +134,47 @@ export default function MessageForm() {
                                     fontSize: "small"
                                 }}
                                 className="font-kode-mono placeholder-input"
+                                type="email"
+                                name="email"
+                                placeholder="Email Address"
+                                required={true}
                                 onChange={(e) => setFormData({ ...formData, email: e.target.value })}
                             />
                         </div>
 
-                        <div className="flex flex-col gap-1 w-full text-white">
+                        <div style={{ position: "relative", marginTop: "20px", zIndex: 0, }} className="flex flex-col gap-1 w-full" >
                             <Card>
-                                <button type="submit" className="flex text-sea-green rounded-lg p-2 gap-2 w-full justify-center items-center font-kode-mono">
-                                    Send <Send size={18} />
-                                </button>
+                                <button type="submit" className="flex text-white rounded-lg gap-2 w-full justify-center items-center font-kode-mono p-12 m-1">Send<Send size={18} /></button>
                             </Card>
                             {showMessageSentAlert && (
                                 <Snackbar
                                     anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-                                    onClose={() => setShowMessageSentAlert(false)}
+                                    onClose={() => { setShowMessageSentAlert(false) }}
                                     open={showMessageSentAlert}
                                     autoHideDuration={6000}
+                                    action={undoSend}
                                     message="Message Sent"
+                                    style={{ position: 'absolute', top: '120%' }}
                                 />
                             )}
                             {showEmailInvalidAlert && (
                                 <Snackbar
                                     anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-                                    onClose={() => setShowEmailInvalidAlert(false)}
+                                    onClose={() => { setShowEmailInvalidAlert(false) }}
                                     open={showEmailInvalidAlert}
                                     autoHideDuration={6000}
-                                    message="Invalid email address"
+                                    message="Invalid Email Address"
+                                    style={{ position: 'absolute', top: '120%' }}
                                 />
                             )}
                             {showMessageNotSentAlert && (
                                 <Snackbar
                                     anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-                                    onClose={() => setShowMessageNotSentAlert(false)}
+                                    onClose={() => { setShowMessageNotSentAlert(false); }}
                                     open={showMessageNotSentAlert}
                                     autoHideDuration={6000}
-                                    message="Message not sent. Please try again."
+                                    message="Message not sent"
+                                    style={{ position: 'absolute', top: '120%' }}
                                 />
                             )}
                         </div>
