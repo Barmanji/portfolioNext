3d7a073fb61dbd2be82847764f0418700aaeda2a
Barmanji <barmanjiaj@gmail.com>
2024-11-01 23:17:40 +0530

    added: colors on project headings

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -46,7 +46,7 @@ export default async function ProjectsPage() {
             Projects
           </h2>
           <p className="mt-4 text-zinc-400">
-            Some of the projects are academic and some are on my own time.
+            My work treasure! Mostly are hobby projects, also hosted.<span className="text-red-500"> ENJOY & EXPLORE! </span>
           </p>
         </div>
         <div className="w-full h-px bg-zinc-800" />
