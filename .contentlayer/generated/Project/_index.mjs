e0840a177bb4e6865b170c366ddbe0276df24880
Ajay Barman <barmanjiaj@gmail.com>
2025-01-07 10:04:57 +0530

    feat: bug fix

--- .contentlayer/generated/Project/_index.mjs
+++ .contentlayer/generated/Project/_index.mjs
@@ -3,6 +3,7 @@
 import projects__bananatalkMdx from './projects__bananatalk.mdx.json' assert { type: 'json' }
 import projects__barmanjiComMdx from './projects__barmanji.com.mdx.json' assert { type: 'json' }
 import projects__birthdayWondersMdx from './projects__birthdayWonders.mdx.json' assert { type: 'json' }
+import projects__calculatorMdx from './projects__calculator.mdx.json' assert { type: 'json' }
 import projects__cashRegisterManagerMdx from './projects__cashRegisterManager.mdx.json' assert { type: 'json' }
 import projects__currency_converterMdx from './projects__currency_converter.mdx.json' assert { type: 'json' }
 import projects__dotfilesMdx from './projects__dotfiles.mdx.json' assert { type: 'json' }
@@ -15,6 +16,5 @@ import projects__reactRouterPracMdx from './projects__reactRouterPrac.mdx.json' 
 import projects__todov1Mdx from './projects__todov1.mdx.json' assert { type: 'json' }
 import projects__todov2Mdx from './projects__todov2.mdx.json' assert { type: 'json' }
 import projects__vbarmanBdayMdx from './projects__vbarman-bday.mdx.json' assert { type: 'json' }
-import projects__calculatorMdx from './projects__calculator.mdx.json' assert { type: 'json' }
 
-export const allProjects = [projects__bananatalkMdx, projects__barmanjiComMdx, projects__birthdayWondersMdx, projects__cashRegisterManagerMdx, projects__currency_converterMdx, projects__dotfilesMdx, projects__dummyMdx, projects__firstProjectMdx, projects__megablogMdx, projects__passwordGeneratorMdx, projects__profitOrLossMdx, projects__reactRouterPracMdx, projects__todov1Mdx, projects__todov2Mdx, projects__vbarmanBdayMdx, projects__calculatorMdx]
+export const allProjects = [projects__bananatalkMdx, projects__barmanjiComMdx, projects__birthdayWondersMdx, projects__calculatorMdx, projects__cashRegisterManagerMdx, projects__currency_converterMdx, projects__dotfilesMdx, projects__dummyMdx, projects__firstProjectMdx, projects__megablogMdx, projects__passwordGeneratorMdx, projects__profitOrLossMdx, projects__reactRouterPracMdx, projects__todov1Mdx, projects__todov2Mdx, projects__vbarmanBdayMdx]
